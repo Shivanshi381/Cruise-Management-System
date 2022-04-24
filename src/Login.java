3d5c5de179aa523f72ca5d75440903fa30d2cb59
SWAPNIL BISWAS <73814033+swapnil-23@users.noreply.github.com>
2022-04-24 11:02:05 +0530

    modified the login class

--- src/Login.java
+++ src/Login.java
@@ -1,6 +1,24 @@
 public class Login {
 
-    public String userID;
-    public String userName;
+    private String userID;
+    private String userName;
     private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void login()
+    {
+
+    }
 }
